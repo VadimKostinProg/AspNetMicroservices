763e7f6f9dbd676743dd56e33456f9f31913645f
VadimKostin <vadkostinwork@gmail.com>
2023-07-08 14:49:31 +0300

    Add entities to Basket.API

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart(string userName)
+        {
+            this.UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal total = this.Items
+                    .Select(item => item.Price * item.Qunatity)
+                    .Sum();
+                return total;
+            }
+        }
+    }
+}
