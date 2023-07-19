6cd4ace54ebf64a241678106bda4a07be321416b
VadimKostin <vadkostinwork@gmail.com>
2023-07-19 10:00:16 +0300

    Add RabbitMQ container and project for EventBus.Messages

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public Guid Id { get; set; }
+        public DateTime CreationTime { get; set; }
+
+        public IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationTime = DateTime.Now;
+        }
+
+        public IntegrationBaseEvent(Guid id, DateTime creationTime)
+        {
+            Id = id;
+            CreationTime = creationTime;
+        }
+    }
+}
