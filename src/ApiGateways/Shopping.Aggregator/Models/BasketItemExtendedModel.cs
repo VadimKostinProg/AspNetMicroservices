fc6e850136cd62e83ddee5d8931ba4a79e9d7e68
VadimKostin <vadkostinwork@gmail.com>
2023-08-09 13:10:38 +0300

    Add DTO models to Shopping.Aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketItemExtendedModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Shopping.Aggregator.Models
+{
+    public class BasketItemExtendedModel
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string ProductId { get; set; } = null!;
+        public string ProductName { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public string Summary { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string ImageFile { get; set; } = null!;
+    }
+}
